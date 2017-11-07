524eae1d6bdc1c2533867095fa1f5d2840c27a8c
Kyle Thomson <kjay.thomson@gmail.com>
2017-11-07 19:19:00 +1100

    Add time element

--- src/index.js
+++ src/index.js
@@ -30,10 +30,26 @@ const styles = {
     borderColor: grey,
     borderRadius: 3
   },
-  tableCell: {
+  calCell: {
     textAlign: "center",
     userSelect: "none",
     width: cellSize
+  },
+  timeCell: {
+    height: "100%",
+    padding: "0 10px",
+    cursor: "pointer",
+    userSelect: "none",
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#ccc",
+    display: "flex",
+    alignItems: "center"
+  },
+  selected: {
+    borderColor: green,
+    backgroundColor: green,
+    color: "white"
   }
 };
 
@@ -74,8 +90,17 @@ export default class DateTimePicker extends React.Component<Props, State> {
   state = {
     value: this.props.initialValue || moment(),
     showCal: true,
-    shownMonth: this.props.initialValue || moment()
+    shownMonth: this.props.initialValue || moment(),
+    showMinSelect: false
   };
+
+  handleHourSelect(hr: number) {
+    this.setState({ value: this.state.value.hour(hr), showMinSelect: true });
+  }
+
+  handleMinSelect(min: number) {
+    this.setState({ value: this.state.value.minute(min) });
+  }
 
   render() {
     const datesInMonthByWeek = getMonthWeeks(
@@ -140,7 +165,7 @@ export default class DateTimePicker extends React.Component<Props, State> {
                 style={{
                   display: "flex",
                   justifyContent: "space-between",
-                  alignContent: "center",
+                  alignItems: "center",
                   width: "100%"
                 }}
               >
@@ -155,7 +180,7 @@ export default class DateTimePicker extends React.Component<Props, State> {
                   {svgs.arrow}
                 </div>
 
-                <span style={{ padding: 10, fontWeight: "bold" }}>
+                <span style={{ fontWeight: "bold" }}>
                   {this.state.shownMonth.format("MMMM YYYY")}
                 </span>
                 <div
@@ -170,14 +195,14 @@ export default class DateTimePicker extends React.Component<Props, State> {
                 </div>
               </div>
               <table
-                style={{ tableLayout: "fixed", borderCollapse: "collapse" }}
+                style={{ tableLayout: "fixed", borderCollapse: "collapse", margin: "15 0", }}
               >
                 <thead>
                   <tr>
                     {daysOfTheWeek.map(day => (
                       <td
                         style={{
-                          ...styles.tableCell,
+                          ...styles.calCell,
                           cursor: "default",
                           color: "#6b6b6b",
                           fontSize: 12
@@ -198,17 +223,13 @@ export default class DateTimePicker extends React.Component<Props, State> {
                             <td
                               key={day.date.date()}
                               style={{
-                                ...styles.tableCell,
+                                ...styles.calCell,
                                 borderWidth: 1,
                                 borderStyle: "solid",
                                 borderColor: "#ccc",
                                 cursor: "pointer",
                                 ...(day.date.isSame(this.state.value, "day")
-                                  ? {
-                                      borderColor: green,
-                                      backgroundColor: green,
-                                      color: "white"
-                                    }
+                                  ? styles.selected
                                   : {})
                               }}
                               onClick={() => this.setState({ value: day.date })}
@@ -223,6 +244,81 @@ export default class DateTimePicker extends React.Component<Props, State> {
                   ))}
                 </tbody>
               </table>
+              <div
+                style={{
+                  width: "100%",
+                  height: 30,
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  alignItems: "center"
+                }}
+              >
+                {this.state.showMinSelect ? (
+                  <div style={{ display: "flex", alignItems: "center"}}>
+                  <div
+                    style={{
+                      ...styles.timeCell,
+                      fontWeight: "bold",
+                      color: green,
+                      borderColor: green,
+                    }}
+                    onClick={() => this.setState({ showMinSelect: false })}
+                  >
+                    {this.state.value.hour()}
+                  </div>
+                  <div style={{ padding: "0 10", fontWeight: "bold"}}>
+                    :
+                  </div>
+                  </div>
+                ) : (
+                  <div style={{ marginRight: 10 }}>Hour</div>
+                )}
+                <div
+                  style={{
+                    flex: 1,
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    overflowX: "scroll",
+                    paddingTop: 10,
+                    paddingBottom: 10
+                    // boxShadow: "inset -10px 0px 5px 1px #aaaaaa"
+                  }}
+                >
+                  {this.state.showMinSelect
+                    ? ["00", "15", "30", "45"].map(min => {
+                        return (
+                          <div
+                            style={{
+                              ...styles.timeCell,
+                              ...(Number(min) === this.state.value.minute()
+                                ? styles.selected
+                                : {})
+                            }}
+                            onClick={() => this.handleMinSelect(Number(min))}
+                          >
+                            {min}
+                          </div>
+                        );
+                      })
+                    : [...Array(24).keys()]
+                        .map(index => index + 1)
+                        .map(hour => (
+                          <div
+                            style={{
+                              ...styles.timeCell,
+                              ...(hour === this.state.value.hour()
+                                ? styles.selected
+                                : {})
+                            }}
+                            onClick={() => this.handleHourSelect(hour)}
+                          >
+                            {hour}
+                          </div>
+                        ))}
+                </div>
+              </div>
             </div>
           </div>
         )}
