c3e1954ee1b3d7d1bdccca3d3964adb966f0d553
Kyle Thomson <kjay.thomson@gmail.com>
2018-10-11 18:24:12 +0200

    Add aliases for components

--- nwb.config.js
+++ nwb.config.js
@@ -1,7 +1,12 @@
+const path = require("path");
+
 module.exports = {
-  type: 'react-component',
+  type: "react-component",
   npm: {
     esModules: true,
     umd: false
+  },
+  aliases: {
+    src: path.resolve("/src")
   }
-}
+};
